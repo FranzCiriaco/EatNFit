edb84d1c629634af4a55c310761ea9229e88f373
FranzCiriaco <franzciriaco1@gmail.com>
2025-06-19 10:08:26 +0800

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,80 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text('EatNFit' , style: TextStyle(
-          fontSize: 35,
-        ),),
-        backgroundColor: Colors.blue[500]
+        title: const Text(
+          'EatNFit',
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+        backgroundColor: Colors.blue[500],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Franz 1', style: TextStyle(
-              fontSize: 60,
-                  fontWeight: FontWeight.w900
-            )),
-            Text('Franz 2', style: TextStyle(
-              fontSize: 90,
-                  fontWeight: FontWeight.w900
-            )),
-            Text('Franz 3', style: TextStyle(
-              fontSize: 60,
-                  fontWeight: FontWeight.w900
-            )),
-        ]
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(16.0), // Margin for Name
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Row(
+              children: const [
+                Text(
+                  'Name: ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Franz Janrey Ciriaco',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20), // Margin for Age
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Row(
+              children: const [
+                Text(
+                  'Age: ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '23 years old',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 5, 30, 15), // Margin for Gender
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Row(
+              children: const [
+                Text(
+                  'Gender: ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Male',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    )
-  )
+    ),
   ));
 }
+
+
+
+
